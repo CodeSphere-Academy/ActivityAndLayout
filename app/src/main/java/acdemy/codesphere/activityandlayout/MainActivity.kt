a58105d0adc3f0114b464e4ae24b2e9f3c090bea
win-than-htike <mmdev147@gmail.com>
2025-05-12 21:17:02 +0700

    Add the lifecycle log in MainActivity

--- app/src/main/java/acdemy/codesphere/activityandlayout/MainActivity.kt
+++ app/src/main/java/acdemy/codesphere/activityandlayout/MainActivity.kt
@@ -1,14 +1,21 @@
 package acdemy.codesphere.activityandlayout
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.v(TAG, "onCreate called")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -17,4 +24,40 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.v(TAG, "onSaveInstanceState called")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.v(TAG, "onRestoreInstanceState called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v(TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(TAG, "onDestroy called")
+    }
+
 }
